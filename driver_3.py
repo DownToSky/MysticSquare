f78d1774eb9364c7c65dcea93cd5e694ec7422f6
downtosky <mme94@scarletmail.rutgers.edu>
2017-02-09 16:51:16 -0500

    just started the main one

---
+++ driver_3.py
@@ -0,0 +1,13 @@
+import sys
+
+HELP_MESSAGE = "This program requires two inputs.\n"
+                "Do:\n"
+                "$ pyhton driver.py <method> <board>"
+
+
+
+
+
+if __name__ = "__main__":
+    if len(sys.argv) != 2:
+        print("This program requres two input ")
